664442bc9e5e9497b1c9f530db4a8506571f6598
chamber0000 <123801663+chamber0000@users.noreply.github.com>
2023-02-04 12:39:30 +0900

    230204

---
+++ 230204/mod1.py
@@ -0,0 +1,14 @@
+# 변수 생성
+x = "module test"
+
+# 함수 생성
+def func_1(a, b):
+    return a+b
+
+# 클래스 생성
+class Class_1():
+    def __init__(self, input_data):
+        self.data = input_data
+
+    def view_data(self):
+        return f"Class에서 입력받은 data의 값은 {self.data}이다"    
---
+++ 230204/mod2/__init__.py
@@ -0,0 +1,4 @@
+x = " module test2"
+
+def func_1(a,b):
+    return a - b
